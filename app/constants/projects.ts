badfa9322065c11b2a10b248e3f4f25ff4bb20c4
Neil Wang <neilywang3@gmail.com>
2024-06-22 08:40:29 -0700

    added languages  & frameworks SVGs

--- app/constants/projects.ts
+++ app/constants/projects.ts
@@ -1,4 +1,25 @@
+import React from "react";
 import { ProjectProps } from "../components/project-display/ProjectProps";
+
+const programming = {
+    Python: "/svg/python.svg",
+    JavaScript: "/svg/javascript.svg",
+    TypeScript: "/svg/typescript.svg",
+    TailwindCSS: "/svg/tailwindcss.svg",
+    React: "/svg/react.svg",
+    NextJS: "/svg/nextjs.svg",
+    TensorFlow: "/svg/tensorflow.svg",
+    PyTorch: "/svg/pytorch.svg",
+    NumPy: "/svg/numpy.svg",
+    Pandas: "/svg/pandas.svg",
+    GCP: "/svg/google-cloud.svg",
+    AWSS3: "/svg/aws-s3.svg",
+    OpenCV: "/svg/opencv.svg",
+    SKLearn: "/svg/scikit-learn.svg",
+    Gemini: "/svg/google-gemini.svg",
+    Jupyter: "/svg/jupyter.svg",
+    MatPlotLib: "/svg/matplotlib.svg",
+}
 
 export const FaceRecognition: ProjectProps = {
     title: "Face Recognition",
@@ -6,8 +27,8 @@ export const FaceRecognition: ProjectProps = {
     projectLink: "https://github.com/GlassesNoGlasses/ComputerVision",
     description: "A face recognition model that uses a convolutional neural network to classify faces.",
     imagePaths: ["https://via.placeholder.com/150"],
-    languages: ["Python"],
-    frameworks: ["TensorFlow", "Keras"]
+    languages: [programming.Python],
+    frameworks: [programming.OpenCV, programming.SKLearn, programming.Pandas, programming.NumPy]
 };
 
 export const FingerPainting: ProjectProps = {
@@ -15,6 +36,8 @@ export const FingerPainting: ProjectProps = {
     subtitle: "AI & Machine Learning",
     projectLink: "https://github.com/GlassesNoGlasses/ComputerVision",
     description: "A finger painting model that uses a convolutional neural network to classify finger paintings.",
+    languages: [programming.Python],
+    frameworks: [programming.OpenCV, programming.Gemini, programming.Pandas, programming.NumPy]
 }
 
 export const SpamClassification: ProjectProps = {
@@ -22,6 +45,8 @@ export const SpamClassification: ProjectProps = {
     subtitle: "AI & Machine Learning",
     projectLink: "https://github.com/GlassesNoGlasses/TFProjects/blob/main/projects/text/SMS_Spam_Classification.ipynb",
     description: "A spam classification model that uses TensorFlow, recurrent neural networks, and LLM procedures to classify spam messages.",
+    languages: [programming.Python],
+    frameworks: [programming.TensorFlow, programming.SKLearn, programming.Jupyter, programming.Pandas, programming.NumPy]
 }
 
 export const SnakeAI: ProjectProps = {
@@ -29,4 +54,6 @@ export const SnakeAI: ProjectProps = {
     subtitle: "AI & Machine Learning",
     projectLink: "https://github.com/GlassesNoGlasses/PythonProjects/tree/main/Snake",
     description: "A spam classification model that uses TensorFlow, recurrent neural networks, and LLM procedures to classify spam messages.",
+    languages: [programming.Python],
+    frameworks: [programming.PyTorch, programming.MatPlotLib, programming.Pandas, programming.NumPy]
 }
