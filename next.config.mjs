4ac03fb1cc1efe3023f95a2376ee3b86cdd9929c
Neil Wang <neilywang3@gmail.com>
2024-06-22 20:18:22 -0700

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+    basePath: "/nextjs-github-pages",
+};
 
 export default nextConfig;
