2fcb16ec63aecbf7952858e35cba51bd69ee5f98
Neil Wang <neilywang3@gmail.com>
2024-06-22 20:27:06 -0700

    removed deployment info

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-    images: {
-        unoptimized: true,
-    },
-    basePath: "/nextjs-github-pages",
 };
 
 export default nextConfig;
